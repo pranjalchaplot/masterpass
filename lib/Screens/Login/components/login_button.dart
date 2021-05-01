e45c6bb3b36440aefa4270f390e7ea9165bdb488
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-05-01 17:46:39 +0530

    add login with verification badge

---
+++ lib/Screens/Login/components/login_button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+
+class LoginButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final Color color, textColor;
+  final IconData icon;
+  const LoginButton({
+    Key key,
+    this.text,
+    this.press,
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.7,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(49),
+        child: Container(
+          alignment: Alignment.center,
+          color: color,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: press,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      text,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 20,
+                        fontFamily: 'SansSerif',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Icon(
+                icon,
+                color: kPrimaryLightColor,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
