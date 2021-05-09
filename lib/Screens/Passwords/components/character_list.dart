3c2c93c4cd6045d75d3b4c2b4092613cb8d34303
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-05-09 23:35:14 +0530

    commenting character view if until requried

--- lib/Screens/Passwords/components/character_list.dart
+++ lib/Screens/Passwords/components/character_list.dart
@@ -1,34 +1,34 @@
-import 'package:flutter/material.dart';
-import 'package:masterpass/Screens/Passwords/components/password_tile.dart';
+// import 'package:flutter/material.dart';
+// import 'package:masterpass/Screens/Passwords/components/password_tile.dart';
 
-class CharacterList extends StatelessWidget {
-  const CharacterList({
-    Key key,
-    @required this.letter,
-  }) : super(key: key);
+// class CharacterList extends StatelessWidget {
+//   const CharacterList({
+//     Key key,
+//     @required this.letter,
+//   }) : super(key: key);
 
-  final letter;
+//   final letter;
 
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: size.width * 0.03),
-      child: Column(
-        children: [
-          Text(
-            letter,
-            style: TextStyle(
-              fontFamily: "TypeFace",
-              fontWeight: FontWeight.bold,
-              fontSize: size.height * 0.03,
-            ),
-          ),
-          SizedBox(height: size.height * 0.02),
-          PasswordTile(),
-          PasswordTile(),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     return Container(
+//       margin: EdgeInsets.symmetric(horizontal: size.width * 0.03),
+//       child: Column(
+//         children: [
+//           Text(
+//             letter,
+//             style: TextStyle(
+//               fontFamily: "TypeFace",
+//               fontWeight: FontWeight.bold,
+//               fontSize: size.height * 0.03,
+//             ),
+//           ),
+//           SizedBox(height: size.height * 0.02),
+//           PasswordTile(),
+//           PasswordTile(),
+//         ],
+//       ),
+//     );
+//   }
+// }
