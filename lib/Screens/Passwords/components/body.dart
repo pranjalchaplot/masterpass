aeee2193d55b7d68f227a57ca11a021cd9a0cac1
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-05-09 23:46:28 +0530

    change statless to  stateful and crud methods

--- lib/Screens/Passwords/components/body.dart
+++ lib/Screens/Passwords/components/body.dart
@@ -1,10 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:masterpass/Screens/Passwords/components/password_tile.dart';
+import 'package:masterpass/Services/crud.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({
     Key key,
   }) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  CrudMethods crudMethods = new CrudMethods();
+  QuerySnapshot passwordsSnapshot;
+
+  @override
+  void initState() {
+    super.initState();
+    crudMethods.getData().then((result) {
+      passwordsSnapshot = result;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
