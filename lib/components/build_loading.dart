3fbf29ba06d75a473231a86ae57b26370d8b11a3
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-06-01 18:21:22 +0530

    add loading screen for multiple screens

---
+++ lib/components/build_loading.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BuildLoading extends StatelessWidget {
+  const BuildLoading({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
